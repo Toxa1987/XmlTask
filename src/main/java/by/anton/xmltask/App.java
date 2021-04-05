fa9315dc8d8c526bb5a919a610da0ac6d9b1b2b0
Anton Pysk <wertyal.87@mail.ru>
2021-04-05 09:17:23 +0300

    add *

---
+++ src/main/java/by/anton/xmltask/App.java
@@ -0,0 +1,52 @@
+package by.anton.xmltask;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException {
+
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+    }
+    private class XmlHandler extends DefaultHandler{
+        @Override
+        public void startDocument() throws SAXException {
+            super.startDocument();
+        }
+
+        @Override
+        public void endDocument() throws SAXException {
+            super.endDocument();
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            super.startElement(uri, localName, qName, attributes);
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            super.characters(ch, start, length);
+        }
+
+        @Override
+        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+            super.ignorableWhitespace(ch, start, length);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            super.endElement(uri, localName, qName);
+        }
+    }
+}
